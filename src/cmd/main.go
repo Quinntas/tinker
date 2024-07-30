ac557a8af8a639e89a110c48856833b0fd87e891
quintas <caioquintassantiago@gmail.com>
2024-07-30 14:42:42 -0300

    Initial commit

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func contactArrIntoString(arr []string) string {
+	var result string
+	for i, v := range arr {
+		if i == 0 {
+			result = v
+		} else {
+			result += " " + v
+		}
+	}
+	return result
+}
+
 func main() {
+	createCommandFlag := flag.Bool("c", false, "Explain the command")
+
+	flag.Parse()
+
+	switch *createCommandFlag {
+	case true:
+		fmt.Println(contactArrIntoString(os.Args[2:]))
+		break
+	case false:
+		fmt.Println(contactArrIntoString(os.Args[1:]))
+		break
+	}
 }
